3ab9bfaebd4946908405a7b6a819548f407507da
jacobshirley <jakeshirley2@gmail.com>
2025-09-21 18:28:54 +0100

    chore: fix test

--- packages/pki-lite/examples/ocsp-request.ts
+++ packages/pki-lite/examples/ocsp-request.ts
@@ -1,20 +1,7 @@
 import { Certificate } from '../src/x509/Certificate.js'
-import { AuthorityInfoAccess } from '../src/x509/extensions/AuthorityInfoAccess.js'
 import { rsaSigningKeys } from '../test-fixtures/signing-keys/rsa-2048/index.js'
 
 const certificate = Certificate.fromPem(rsaSigningKeys.certPem)
-const aias = certificate.tbsCertificate.getExtensionsByName(
-    'AUTHORITY_INFO_ACCESS',
-)
-
-for (const aia of aias) {
-    console.log(
-        'AIAs:',
-        aia.extnValue
-            .parseAs(AuthorityInfoAccess)
-            .map((ad) => ad.accessLocation),
-    )
-}
 
 // NB: This will only work if the OCSP responder is accessible
 const ocsp = await certificate.requestOcsp({
