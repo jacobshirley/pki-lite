ee217a5860aa9443120662f5ce00e9aa8657ca3b
jacobshirley <jakeshirley2@gmail.com>
2025-09-21 16:32:43 +0100

    feat: new tests

--- packages/pki-lite/examples/crl-request.ts
+++ packages/pki-lite/examples/crl-request.ts
@@ -2,39 +2,23 @@ import { Certificate } from '../src/x509/Certificate.js'
 import { CRLDistributionPoints } from '../src/x509/extensions/CRLDistributionPoints.js'
 import { rsaSigningKeys } from '../test-fixtures/signing-keys/rsa-2048/index.js'
 
-const _fetch = globalThis.fetch
-globalThis.fetch = (url, init) => {
-    return new Promise((resolve, reject) => {
-        if (url === 'http://localhost:8080/ca.crl') {
-            // Example CRL in PEM format (replace with a real CRL for actual use)
-            resolve(new Response(rsaSigningKeys.caCrl, { status: 200 }))
-        } else {
-            reject(new Error('Not Found'))
-        }
-    })
+const certificate = Certificate.fromPem(rsaSigningKeys.certPem)
+const crlDps = certificate.tbsCertificate.getExtensionsByName(
+    'CRL_DISTRIBUTION_POINTS',
+)
+
+for (const dp of crlDps) {
+    const crl = dp.extnValue.parseAs(CRLDistributionPoints)
+    console.log(
+        'CRL Distribution Point Extension:',
+        crl[0].distributionPoint?.[0].toHumanString(),
+    )
 }
 
-async function requestCrl() {
-    const certificate = Certificate.fromPem(rsaSigningKeys.certPem)
-    const crlDps = certificate.tbsCertificate.getExtensionsByName(
-        'CRL_DISTRIBUTION_POINTS',
-    )
-
-    for (const dp of crlDps) {
-        const crl = dp.extnValue.parseAs(CRLDistributionPoints)
-        console.log(
-            'CRL Distribution Point Extension:',
-            crl[0].distributionPoint?.[0].toHumanString(),
-        )
-    }
-
-    const crl = await certificate.requestCrl()
-    if (!crl) {
-        console.log('No CRL found')
-        return
-    }
+// NB: This will only work if the CRL distribution point is accessible
+const crl = await certificate.requestCrl()
+if (!crl) {
+    console.log('No CRL found')
+} else {
     console.log('Successfully found CRL', crl.toString())
 }
-
-await requestCrl()
-globalThis.fetch = _fetch
--- packages/pki-lite/examples/ocsp-request.ts
+++ packages/pki-lite/examples/ocsp-request.ts
@@ -2,42 +2,27 @@ import { Certificate } from '../src/x509/Certificate.js'
 import { AuthorityInfoAccess } from '../src/x509/extensions/AuthorityInfoAccess.js'
 import { rsaSigningKeys } from '../test-fixtures/signing-keys/rsa-2048/index.js'
 
-const _fetch = globalThis.fetch
-globalThis.fetch = (url, init) => {
-    return new Promise((resolve, reject) => {
-        if (url === 'http://localhost:8080/ocsp') {
-            resolve(new Response(rsaSigningKeys.ocspResponse, { status: 200 }))
-        } else {
-            reject(new Error('Not Found: ' + url))
-        }
-    })
+const certificate = Certificate.fromPem(rsaSigningKeys.certPem)
+const aias = certificate.tbsCertificate.getExtensionsByName(
+    'AUTHORITY_INFO_ACCESS',
+)
+
+for (const aia of aias) {
+    console.log(
+        'AIAs:',
+        aia.extnValue
+            .parseAs(AuthorityInfoAccess)
+            .map((ad) => ad.accessLocation),
+    )
 }
 
-async function requestOcsp() {
-    const certificate = Certificate.fromPem(rsaSigningKeys.certPem)
-    const aias = certificate.tbsCertificate.getExtensionsByName(
-        'AUTHORITY_INFO_ACCESS',
-    )
+// NB: This will only work if the OCSP responder is accessible
+const ocsp = await certificate.requestOcsp({
+    issuerCertificate: Certificate.fromPem(rsaSigningKeys.caCertPem),
+})
 
-    for (const aia of aias) {
-        console.log(
-            'AIAs:',
-            aia.extnValue
-                .parseAs(AuthorityInfoAccess)
-                .map((ad) => ad.accessLocation),
-        )
-    }
-
-    const ocsp = await certificate.requestOcsp({
-        issuerCertificate: Certificate.fromPem(rsaSigningKeys.caCertPem),
-    })
-
-    if (!ocsp) {
-        console.log('No OCSP response found')
-        return
-    }
+if (!ocsp) {
+    console.log('No OCSP response found')
+} else {
     console.log('OCSP Response:', ocsp.toString())
 }
-
-await requestOcsp()
-globalThis.fetch = _fetch
