088607601cef9241726098246726e5ee3f5dd53c
jacobshirley <jakeshirley2@gmail.com>
2025-09-27 13:14:39 +0100

    chore: allow all params to version

--- scripts/prerelease-version.sh
+++ scripts/prerelease-version.sh
@@ -11,12 +11,13 @@ if [ -z "$1" ]; then
   echo "Example: $0 patch"
   echo "Example: $0 1.2.3"
   echo "Example: $0 major"
+  echo "Example: $0 premajor --preid=beta"
   exit 1
 fi
 
 # Bump version in root package.json
-pnpm version --no-git-tag-version $1
+pnpm version --no-git-tag-version $@
 
 # Bump version in all workspaces
-pnpm -r exec pnpm version --no-git-tag-version $1
+pnpm -r exec pnpm version --no-git-tag-version $@
 
