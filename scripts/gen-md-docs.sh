7e97ae5b88b7c6ed3ccf18886d292fa489c7bca6
Jacob Shirley <jakeshirley2@gmail.com>
2025-09-28 12:56:40 +0100

    chore: change docs link in markdown to versioned link (#18)
    * chore: change docs link in markdown to versioned link
    
    * fix: search correct folder

---
+++ scripts/gen-md-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# Get latest version from package.json
+NEW_VERSION=$(node -p "require('./package.json').version")
+echo "Generating docs for version: $NEW_VERSION"
+
+# Update absolute doc links to versioned path
+find . -type f -name '*.md' -exec sed -i "s|https://jacobshirley.github.io/pki-lite|https://jacobshirley.github.io/pki-lite/v$NEW_VERSION|g" {} +
+
+# Generate markdown docs
+pnpm exec typedoc --options typedoc.json --plugin typedoc-plugin-markdown --readme none && pnpm format
