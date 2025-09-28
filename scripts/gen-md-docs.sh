ac616eebd606b0af1b810679022745882ebde50d
jacobshirley <jakeshirley2@gmail.com>
2025-09-28 13:39:32 +0100

    fix: replace version

--- scripts/gen-md-docs.sh
+++ scripts/gen-md-docs.sh
@@ -7,7 +7,7 @@ NEW_VERSION=$(node -p "require('./package.json').version")
 echo "Generating docs for version: $NEW_VERSION"
 
 # Update absolute doc links to versioned path
-find . -type f -name '*.md' -exec sed -i "s|https://jacobshirley.github.io/pki-lite|https://jacobshirley.github.io/pki-lite/v$NEW_VERSION|g" {} +
+find . -type f -name '*.md' -exec sed -i "s|https://jacobshirley.github.io/pki-lite/v[0-9a-zA-Z._-]*|https://jacobshirley.github.io/pki-lite/v$NEW_VERSION|g" {} +
 
 # Generate markdown docs
 pnpm exec typedoc --options typedoc.json --plugin typedoc-plugin-markdown --readme none && pnpm format
