6c42910d05e180e1c1d258632c6ebf43850b3237
jacobshirley <jakeshirley2@gmail.com>
2025-09-28 12:01:55 +0100

    docs: publish versions

--- scripts/gen-html-docs.sh
+++ scripts/gen-html-docs.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# Get latest version from package.json
+NEW_VERSION=$(node -p "require('./package.json').version")
+echo "Generating docs for version: $NEW_VERSION"
+
 pnpm exec typedoc \
     --options typedoc.json \
     --out docs-html \
@@ -6,3 +14,6 @@ pnpm exec typedoc \
     --projectDocuments 'EXAMPLES.md' \
     --githubPages \
     --includeVersion
+
+# Copy to versioned folder
+cp -r docs-html docs-html/v$NEW_VERSION
